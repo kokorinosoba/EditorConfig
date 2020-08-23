6e35776b6bac280ef43413ceb4a64a3f8ab91ab0
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2020-08-23 15:46:11 +0900

    プロジェクト ファイルを追加します。

---
+++ EditorConfig/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EditorConfig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
